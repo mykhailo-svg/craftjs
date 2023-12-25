803861f2427583874d82f429eaa71a31e69eb3f7
Mykhailo Vydryk <wyzdrykm@gmail.com>
2023-12-25 10:12:40 +0200

    Fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Editor, Frame, Element } from "@craftjs/core/src";
+import { Editor, Frame, Element } from "@craftjs/core";
 import { createMuiTheme } from "@material-ui/core";
 import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
 
-import { Viewport, RenderNode } from "../components/editor";
-import { Container, Text } from "../components/selectors";
-import { Button } from "../components/selectors/Button";
-import { Custom1, OnlyButtons } from "../components/selectors/Custom1";
-import { Custom2, Custom2VideoDrop } from "../components/selectors/Custom2";
-import { Custom3, Custom3BtnDrop } from "../components/selectors/Custom3";
-import { Video } from "../components/selectors/Video";
+import { Viewport, RenderNode } from "./components/editor";
+import { Container, Text } from "./components/selectors";
+import { Button } from "./components/selectors/Button";
+import { Custom1, OnlyButtons } from "./components/selectors/Custom1";
+import { Custom2, Custom2VideoDrop } from "./components/selectors/Custom2";
+import { Custom3, Custom3BtnDrop } from "./components/selectors/Custom3";
+import { Video } from "./components/selectors/Video";
 
 const theme = createMuiTheme({
   typography: {
